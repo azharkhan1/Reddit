17fe7b1cbc855ff89871cbe56477a9eb481448bb
hassan <hassanribery0335@gmail.com>
2021-02-18 14:06:15 +0500

    done

---
+++ src/components/signup/signup.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Link,
+    Route
+} from 'react-router-dom'
+
+
+function Signup() {
+    return(
+        <div>
+            <h1>
+            Signup
+            </h1> 
+        </div>
+    )
+    
+}
+export default Signup;;
